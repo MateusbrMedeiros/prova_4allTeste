4feb80dc0a9a1c314a8709eebd53b410d751479c
MateusbrMedeiros <75047939+MateusbrMedeiros@users.noreply.github.com>
2020-11-25 21:48:55 -0300

    initial files

---
+++ prova_4all/src/main/java/com/medeiros/prova_4all/Prova4allApplication.java
@@ -0,0 +1,13 @@
+package com.medeiros.prova_4all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prova4allApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prova4allApplication.class, args);
+	}
+
+}
---
+++ prova_4all/src/test/java/com/medeiros/prova_4all/Prova4allApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medeiros.prova_4all;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prova4allApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
